2e575b2d2e912ad03570cf6410b4b1542976a9ce
mlewartoski <mlewartoski@ciandt.com>
2022-07-19 10:13:34 -0300

    testes, task 0 #0

---
+++ 0x00/email/Pessoa.java
@@ -0,0 +1,11 @@
+package email;
+
+public class Pessoa {
+    public static boolean emailValid(String email){
+        if (!email.contains("@"))
+            return false;
+        if (email.length() > 50)
+            return false;
+        return true;
+    }
+}
